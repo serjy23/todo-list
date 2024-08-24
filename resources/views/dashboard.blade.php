93bda9782672624b857f508ca73867b9abda7605
Fafikk <fafikkdev@gmail.com>
2024-08-24 14:49:18 +0200

    initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>To-Do List Dashboard</title>
+</head>
+<body>
+
+    <p>
+        This is an exaple to-do list dashboard
+    </p>
+    <a href="{{ url('/') }}">
+        <button>
+            Return to index
+        </button>
+    </a>
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>To-Do List</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body>
+
+    <p>
+        This is a simple to-do list app.
+    </p>
+    <a href="{{ url('/dashboard') }}">
+        <button>
+            Start
+        </button>
+    </a>
+
+</body>
+</html>
