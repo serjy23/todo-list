6820625eab511aa55de0c959d505d18baad3f833
Fafikk <fafikkdev@gmail.com>
2024-08-26 20:52:18 +0200

    fix test error

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/dashboard', '200');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
