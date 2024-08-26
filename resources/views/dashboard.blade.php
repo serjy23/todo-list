749d41e651c1310e5bc94c7e46aba75134aedfbb
Fafikk <fafikkdev@gmail.com>
2024-08-26 20:34:11 +0200

    new dashboard css

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<title>{{ config('app.name') }}</title>
+        
+		<!-- Roboto font embed -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.5.2/css/all.css">
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/reset.min.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+	</head>
+	<body>
+
+		<header>
+			<form action="/item" method="POST">
+                <input type="text" placeholder="Enter an activity.." name="item" required>
+			    <button id="add">
+				    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve"><g><path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/></g></svg>
+			    </button>
+            </form>
+		</header>
+
+		<div class="container">
+			<!-- Tasks to-do -->
+			<ul class="todo" id="todo">
+                <li>
+                    test
+                    <form action="/item" method="post">
+                        @method('DELETE')
+                        <button class="delete">
+                            <i class="fa-light fa-trash-can"></i>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+		</div>
+
+        <footer>
+            <span>Made with <i class="fa-solid fa-heart"></i> by <a href="https://example.com/" target="_blank">Fafik</a></span>
+            <span><a href="https://github.com/Fafikk/todo-list/" target="_blank">Github</a></span>
+        </footer>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/dashboard');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
+
+// TODO: item controller
