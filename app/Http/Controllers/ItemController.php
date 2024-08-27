b21a0a8d742e6af9f7c95fcd0efe2da40320ae0f
Fafikk <fafikkdev@gmail.com>
2024-08-27 17:44:29 +0200

    get records from db + create dashboard controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function insert() {
+        // $task = new Task();
+        // $task->name = "Test";
+        // $task->save();
 
+        return view('welcome');
     }
 
     public function delete() {
         
+
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ItemController;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+Route::get('/dashboard', [ DashboardController::class, 'view' ]);
 
 Route::post('/item', [ ItemController::class, 'insert' ]);
 Route::delete('/item', [ ItemController::class, 'delete' ]);
